272431423e237eb6b07041b2d65056f1e046715c
demiddima <demid.dima.1996@gmail.com>
2024-02-15 19:57:09 +1000

    add WorkList

--- ZennoHeplerV2/WorkList.cs
+++ ZennoHeplerV2/WorkList.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Global.ZennoExtensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary.Enums.Log;
 using ZennoLab.InterfacesLibrary.ProjectModel;
 
 namespace ZennoHelperV2
@@ -39,6 +41,55 @@ namespace ZennoHelperV2
             }
             return count;
         }
+
+        /// <summary>
+        /// Удаление строк по значению
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int DeleteRow(IZennoList list, string value)
+        {
+            int count = 0;
+            lock (SyncObjects.ListSyncer)
+            {
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    if (list[i].Contains(value))
+                    {
+                        list.RemoveAt(i);
+                        count++;
+                        i--;
+                    }
+                }
+            }
+            project.SendToLog($"Завершение работы метода DeleteRowContains", LogType.Info, false, LogColor.Gray);
+            return count;
+        }
+
+        /// <summary>
+        /// Получение строки по значению
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string GetItemByValue(IZennoList list, string value)
+        {
+            lock (SyncObjects.TableSyncer)
+            {
+                foreach (var item in list)
+                {
+                    if (item.Contains(value))
+                    {
+                        project.SendToLog($"Завершение работы метода GetItemByValue", LogType.Info, false, LogColor.Gray);
+                        return item;
+                    }
+                }
+            }
+            project.SendToLog($"Завершение работы метода GetItemByValue c результатом null", LogType.Info, false, LogColor.Gray);
+            return null;
+        }
+
 
     }
 }
